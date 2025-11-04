f8f02cef6517efe7de7db0f3a3c891e13095073e
Mdarafath07 <mdarafathuddin10@gmail.com>
2025-11-04 21:58:17 +0600

    Use StreamBuilder for real-time score updates

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,143 +7,164 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   List<FootballMatch> _matchList = [];
-  bool _inProgress = false;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _getFootballMatches();
-  }
 
-  Future<void> _getFootballMatches() async {
-    _inProgress = true;
-    setState(() {
 
-    });
-    _matchList.clear();
-    final snapshot = await FirebaseFirestore.instance
-        .collection("football")
-        .get();
-    for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshot.docs) {
-      _matchList.add(FootballMatch(
-          id: doc.id,
-          team1: doc.get("team1_name"),
-          team1Score: doc.get("team1_score"),
-          team2: doc.get("team2_name"),
-          team2Score: doc.get("team2_score"),
-          isRunning: doc.get("is_running"),
-          winner: doc.get("winner_team")));
-    }
-    _inProgress = false;
-    setState(() {
 
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(title: Text("Live score")),
-      body: Visibility(
-        visible: _inProgress==false,
-        replacement: Center(
-          child: CircularProgressIndicator(),
-        ),
-        child: ListView.builder(
-          itemCount: _matchList.length,
-          itemBuilder: (context, index) {
-            final footballMatch = _matchList[index];
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-              child: Container(
-                alignment: Alignment.center,
-                height: 80,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Row(
-                  children: [
-                    SizedBox(width: 10,),
-                    CircleAvatar(backgroundColor: footballMatch.isRunning ? Colors.red : Colors.grey,
-                      radius: 5,),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text("${footballMatch.team1}",style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                    ),),
-                                    Text("${footballMatch.team1Score}",style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20,
-                                    ),),
-                                  ],
-                                ),
-                                Text("VS",style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w900,
-                                ),),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text("${footballMatch.team2}",style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("football").snapshots(),
+        builder: (context, snapshots) {
+          if (snapshots.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshots.hasError) {
+            return Center(child: Text(snapshots.error.toString()));
+          } else if (snapshots.hasData) {
+            _matchList.clear();
+            for (QueryDocumentSnapshot<Map<String, dynamic>> doc
+                in snapshots.data!.docs) {
+              _matchList.add(
+                FootballMatch(
+                  id: doc.id,
+                  team1: doc.get("team1_name"),
+                  team1Score: doc.get("team1_score"),
+                  team2: doc.get("team2_name"),
+                  team2Score: doc.get("team2_score"),
+                  isRunning: doc.get("is_running"),
+                  winner: doc.get("winner_team"),
+                ),
+              );
+            }
 
-                                    ),),
-                                    Text("${footballMatch.team2Score}",style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 20,
-                                    ),),
+            return ListView.builder(
+              itemCount: _matchList.length,
+              itemBuilder: (context, index) {
+                final footballMatch = _matchList[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 80,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 10),
+                        CircleAvatar(
+                          backgroundColor: footballMatch.isRunning
+                              ? Colors.red
+                              : Colors.grey,
+                          radius: 5,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "${footballMatch.team1}",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        Text(
+                                          "${footballMatch.team1Score}",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      "VS",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "${footballMatch.team2}",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        Text(
+                                          "${footballMatch.team2Score}",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ],
                                 ),
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("Winner : ",style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 10,
-                                fontWeight: FontWeight.w600,
-                              ),),
-                              Text("${footballMatch.winner}",style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 10,
-                                fontWeight: FontWeight.w600,
-                              ),),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Winner : ",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    "${footballMatch.winner}",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10),
                             ],
                           ),
-                          SizedBox(height: 10,),
-                        ],
-                      ),
-                    )
-
-                  ],
-                ),
-
-              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             );
-          },
-        ),
+          }
+          return SizedBox();
+        },
       ),
     );
   }
@@ -168,7 +189,3 @@ class FootballMatch {
     required this.winner,
   });
 }
-
-
-
-
